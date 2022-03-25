2400033008b8dbe91345029021a78f5b42331cb6
Sandy Raja Alamsyah <ikandarat123@gmail.com>
2022-03-25 10:14:26 +0700

    finish

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      heading: "AMVID",
+      description: "AMVID is a community to creating AMV/MAD",
+    };
+  },
+});
